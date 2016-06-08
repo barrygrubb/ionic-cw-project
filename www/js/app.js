40539a1de33a00cbade208135467c55a46119a95
barrygrubb <barrygrubb83@gmail.com>
2016-06-08 16:27:46 +0100

    Extract index page view in to a template, and add another template for the details view. Display interests of selected person in details view.

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,48 @@
 (function() {
   angular.module('cwapp', ['ionic'])
 
+    .config(function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'templates/homeView.html',
+          controller: 'ContactsController as contacts'
+        })
+        .state('details', {
+          url: '/details',
+          templateUrl: 'templates/detailView.html',
+          controller: 'ContactsController as contacts'
+        })
+
+      $urlRouterProvider.otherwise("/");
+    })
+
     .factory("Contacts", contactsFactory)
     .controller("ContactsController", contactsController);
 
+
     function contactsController(Contacts) {
       var vm = this;
-      Contacts.then(function success(response) {
+      vm.set = Contacts.set;
+      vm.get = Contacts.get();
+      Contacts.getAllContacts.then(function success(response) {
         vm.data = response.data;
+        // console.log(vm.data[0]);
       })
     }
 
     function contactsFactory($http) {
-      return $http.get('https://cw-project-backend.herokuapp.com/getRecords');
+      var selectedContact = {}
+      return {
+        getAllContacts: $http.get('https://cw-project-backend.herokuapp.com/getRecords'),
+        set: function(data) {
+          selectedContact = data;
+        },
+        get: function() {
+          console.log(selectedContact);
+          return selectedContact;
+        }
+    }
     }
 })();
 
