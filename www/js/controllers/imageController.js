ddf632e0a1941f101dc64d21f24d6bbd5b94e8ed
Tom Pickard <tcpickard94@gmail.com>
2016-06-20 17:44:57 +0100

    Images now successfully upload to firebase
    - Changed camera destination type to DATA_URL which returns a base64
      encoded image.
    - Added a function to convert the base64 string to a blob, which is then
      passed to the firebase upload function.
    - Added a function to create a randomly generated string to use as a
      name for each uploaded image.
    Image controller is rather messy and can certainly be refactored

--- www/js/controllers/imageController.js
+++ www/js/controllers/imageController.js
@@ -3,7 +3,7 @@
 
     .controller("imageController", function($ionicActionSheet, $cordovaCamera) {
       var config = {
-        apiKey: "GMYnb01kjIFJ6Ymxoa0KvBw4GBwhTKmryqB8D52l",
+        apiKey: "AIzaSyCG_LD6NqPbBwKTibm4Alr0H47IdJFMKKM",
         storageBucket: "circular-wave-project.appspot.com",
       };
 
@@ -15,8 +15,9 @@
       function setOptions(srcType) {
         var options = {
           sourceType: srcType,
-          targetWidth: 200,
-          targetHeight: 200
+          destinationType: Camera.DestinationType.DATA_URL,
+          targetWidth: 300,
+          targetHeight: 400
         };
         return options;
       }
@@ -32,20 +33,56 @@
           cancelText: 'Cancel',
           buttonClicked: function(index) {
             var srcType;
+            var dataUrl;
             if (index === 0) {
               srcType = Camera.PictureSourceType.CAMERA;
 
               $cordovaCamera.getPicture(setOptions(srcType)).then(function(imageData){
-                var blob = new Blob([imageData], {type: "image/jpg"});
-                var uploadTask = ref.child("images/filename.jpg").put(blob);
+                function b64toBlob(b64Data, contentType, sliceSize) {
+                  contentType = contentType || '';
+                  sliceSize = sliceSize || 512;
 
+                  var byteCharacters = atob(b64Data);
+                  var byteArrays = [];
+
+                  for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+                    var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+                    var byteNumbers = new Array(slice.length);
+                    for (var i = 0; i < slice.length; i++) {
+                      byteNumbers[i] = slice.charCodeAt(i);
+                    }
+
+                    var byteArray = new Uint8Array(byteNumbers);
+
+                    byteArrays.push(byteArray);
+                  }
+
+
+
+                  var blob = new Blob(byteArrays, {type: contentType});
+                  return blob;
+                }
+                var blob = b64toBlob(imageData, 'image/jpeg');
+
+                function makeid() {
+                  var text = '';
+                  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+                  for (var i = 0; i < 5; i++) {
+                    text += possible.charAt(Math.floor(Math.random() * possible.length));
+                  }
+                  return text;
+                }
+
+                var filename = makeid();
+                var uploadTask = ref.child("images/" + filename +  ".jpg").put(blob);
                 uploadTask.on('state_changed', function(snapshot){
 
                 }, function(error){
                   console.log("ERROR!!!!!" + error.serverResponse);
                 }, function(){
                   var downloadURL = uploadTask.snapshot.downloadURL;
-                  console.log("-------" + downloadURL);
                 });
               }, function cameraError(error) {
                 console.debug("Unable to obtain picture: " + error, "app");
